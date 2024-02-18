6988cbc639b2cf349ff02ef068abc2fb3678aab2
ikari2k <pluta.m@gmail.com>
2024-02-18 20:48:11 +0100

    alien and bullets are being removed from screen upon  collision

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,6 +79,10 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        # Check if any bullet hit the alien
+        # If yes, remove both alien and bullet
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
     def _create_fleet(self):
         """Create alien fleet"""
         # Create an alien
