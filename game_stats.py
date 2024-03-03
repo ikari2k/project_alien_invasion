eae363e5b6d3a2d81607d72d8dbafb2ac51baf99
ikari2k <pluta.m@gmail.com>
2024-03-03 20:23:29 +0100

    high score setting

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # Highest score should never be zeroed
+        self.high_score = 0
+
     def reset_stats(self):
         """Initialize games stats that can change in the course of the game"""
         self.ships_left = self.settings.ship_limit
