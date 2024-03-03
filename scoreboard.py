2ae7f0c71a39538441d7b8cb60d68c475347358c
ikari2k <pluta.m@gmail.com>
2024-03-03 20:22:19 +0100

    implemented high score

--- scoreboard.py
+++ scoreboard.py
@@ -17,6 +17,7 @@ class Scoreboard:
 
         # Prepare initial images with scores
         self.prep_score()
+        self.prep_high_score()
 
     def prep_score(self):
         """Turn score into generated image"""
@@ -32,6 +33,25 @@ class Scoreboard:
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """Convert highest score into generated image"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = f"{high_score:,}"
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.settings.bg_color
+        )
+        # Display score in upper center
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
     def show_score(self):
         """Display score on the screen"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+
+    def check_high_score(self):
+        """Check if we reached new high score"""
+        if self.stats.score > self.stats.high_score:
+            self.stats.high_score = self.stats.score
+            self.prep_high_score()
