c4f5bbbbd633e92246c3660d20a161fcd198a763
ikari2k <pluta.m@gmail.com>
2024-03-03 20:11:53 +0100

    rounding up score and better formatting

--- scoreboard.py
+++ scoreboard.py
@@ -20,7 +20,9 @@ class Scoreboard:
 
     def prep_score(self):
         """Turn score into generated image"""
-        score_str = str(self.stats.score)
+        # Round score to nearest multiple of 10
+        rounded_score = round(self.stats.score, -1)
+        score_str = f"{rounded_score:,}"
         self.score_image = self.font.render(
             score_str, True, self.text_color, self.settings.bg_color
         )
